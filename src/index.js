706b5e3e8c34e21203bd876b4024ac59d80b0bfe
Nasreddine Bouchemel <bouchemelnasreddine@gmail.com>
2024-05-17 17:08:04 +0200

    integrated auth0 protection middlewar

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
  */
 
 const express = require("express");
+const { auth } = require("express-openid-connect");
 const path = require("path");
 
 require("dotenv").config();
@@ -24,6 +25,21 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "..", "public")));
 
+
+/**
+ * AUTH0 Configuration
+ */
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUHT0_ISSUER_BASE_URL
+};
+
+
 /**
  * Routes Definitions
  */
@@ -33,6 +49,14 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 app.get("/", (req, res) => {
   res.render("home", { activeRoute: req.originalUrl });
 });
+
+// AUTH0 middleware
+
+app.use(auth(config));
+
+app.use( (req,res, next) => {
+  req.oidc.isAuthenticated() ? next() : res.send('not authenticated')
+} )
 
 // > Profile
 
